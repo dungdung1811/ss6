a9c9c6174521e247759fe8275357bc16fdf9a28b
dungdung1811 <nguyendung181192@gmail.com>
2023-11-12 21:48:08 +0700

    BT_SS6

---
+++ src/main/java/com/example/bt/DiscountCalculatorServlet.java
@@ -0,0 +1,33 @@
+package com.example.bt;
+import java.io.*;
+import javax.servlet.ServletException;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+
+@WebServlet(name = "DiscountCalculatorServlet", value = "/discountCalculatorServlet")
+public class DiscountCalculatorServlet extends HttpServlet {
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.getRequestDispatcher("discountCal.jsp").forward(request,response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        double p = Double.parseDouble(req.getParameter("price"));
+        double pc =  Double.parseDouble(req.getParameter("discountPercent"));
+        double da = p * pc * 0.01;
+        System.out.println(da);
+        req.setAttribute("discount",da);
+        req.getRequestDispatcher("display-discount.jsp").forward(req,resp);
+
+    }
+
+    @Override
+    public void destroy() {
+    }
+}
